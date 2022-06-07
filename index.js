50b23dc4f67f9483dab7b00b3722275260ba0b8f
yeeed711 <rladpwl910@naver.com>
2022-06-08 02:14:46 +0900

    docs: 전역변수 정리

--- index.js
+++ index.js
@@ -37,3 +37,12 @@ let dataBase = [
     count: 3,
   },
 ];
+
+// 변수 정리
+const drinkItems = document.querySelector(".vending-items"); // 자판기 아이템 부분
+const seletItems = document.querySelector(".drink-items"); //카트 아이템 리스트
+const returnBtn = document.querySelector(".return__btn"); // 거스름돈 반환 버튼
+const putBtn = document.querySelector(".put__btn"); // 입금 버튼
+const getBtn = document.querySelector(".get__btn"); // 획득 버튼
+const leftMoney = document.querySelector(".buy-balance span:first-child"); // 잔액
+const myMoney = document.querySelector(".mymoney__text"); // 소지금
