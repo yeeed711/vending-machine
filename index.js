c72f4dd7ba175c11dc3d6d235373986f5db418c7
yeeed711 <rladpwl910@naver.com>
2022-06-08 02:13:45 +0900

    Feat: 추가 database

---
+++ index.js
@@ -0,0 +1,39 @@
+// 아이템 정보들 객체로 배열에 담기
+let dataBase = [
+  {
+    name: "Original_cola",
+    img: "./img/cola.svg",
+    price: 1000,
+    count: 3,
+  },
+  {
+    name: "Violet_cola",
+    img: "./img/violet_cola.svg",
+    price: 1000,
+    count: 3,
+  },
+  {
+    name: "Yellow_cola",
+    img: "./img/yellow_cola.svg",
+    price: 1000,
+    count: 3,
+  },
+  {
+    name: "Cool_cola",
+    img: "./img/cool_cola.svg",
+    price: 1000,
+    count: 3,
+  },
+  {
+    name: "Green_cola",
+    img: "./img/green_cola.svg",
+    price: 1000,
+    count: 3,
+  },
+  {
+    name: "Orange_cola",
+    img: "./img/orange_cola.svg",
+    price: 1000,
+    count: 3,
+  },
+];
