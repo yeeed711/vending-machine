50a4160cc26e65bf33a4701adf696dd705770ae0
yeeed711 <rladpwl910@naver.com>
2022-11-23 19:29:30 +0900

    ♻️ refactort: 상수분리

---
+++ src/constants/index.js
@@ -0,0 +1,6 @@
+export const ACTION_KEY = 'Enter';
+
+// VALIDATE_MESSAGE
+export const VALID_INPUT_VALUE = '입금액을 입력해주세요.';
+export const VALID_MY_MONEY = '소지금이 부족합니다.';
+export const VALID_BALANCE = '잔액이 부족합니다.';
