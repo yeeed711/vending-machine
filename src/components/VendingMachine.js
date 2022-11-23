72072438254fba557735031e1fc22ab87502a44c
yeeed711 <rladpwl910@naver.com>
2022-11-23 18:12:32 +0900

    ✨ feat: vandingMachinePanelClick 기능 구현

--- src/components/VendingMachine.js
+++ src/components/VendingMachine.js
@@ -27,6 +27,7 @@ class VendingMachine {
     setEvent() {
         this.putInMoneyButton();
         this.returnMoneyButton();
+        this.vandingMachinePanelClick();
     }
 
     putInMoneyButton() {
@@ -86,6 +87,54 @@ class VendingMachine {
             } else {
                 this.$myMoney.textContent = formatter(myMoneyValue + balanceValue);
                 this.$balance.textContent = +0;
+            }
+        });
+    }
+
+    vandingMachinePanelClick() {
+        this.$vendingItems.addEventListener('click', (e) => {
+            if (e.target.tagName === 'UL') return;
+
+            const clickedItem = e.target.closest('button');
+            //const selectedItems = Array.from(this.$selectList.querySelectorAll('button'));
+            const selectedItems = selectElAll(this.$selectList, 'button');
+            const clickedItemprice = parseInt(clickedItem.dataset.price);
+            let isFirstClicked = false;
+            const balanceValue = parseInt(this.$balance.textContent.replace(',', '')); //type number
+
+            // 잔액이 클릭한 상품의 금액보다 많을 때만 실행
+            if (balanceValue >= clickedItemprice) {
+                for (const item of selectedItems) {
+                    // 클릭한 상품과 같은 상품이 이미 존재한다면 카운트만 증가
+                    if (item.parentElement.dataset.name === clickedItem.dataset.name) {
+                        if (clickedItem.dataset.count === '0') {
+                            return; // 품절 상품 클릭시 카운트 증가 방지
+                        }
+                        item.children[2].textContent++;
+
+                        // 나중에 클릭한 상품도 계속 위로 올라오도록 처리
+                        this.$selectList.prepend(item.parentElement);
+                        isFirstClicked = true;
+                        break; // return으로 처리하면 아래 코드들이 실행되지 않고 끝나버린다.
+                    }
+                }
+
+                // 처음 클릭한 상품이라면 상품 등록
+                if (!isFirstClicked) {
+                    this.$selectList.prepend(this.clickedItemGenerator(clickedItem));
+                }
+
+                // 클릭한 상품 갯수 감소(품절처리를위해)
+                clickedItem.dataset.count--;
+                console.log(typeof clickedItem.dataset.count);
+                if (clickedItem.dataset.count === '0') {
+                    clickedItem.classList.add('sold-out');
+                }
+                this.$balance.textContent = formatter(balanceValue - clickedItemprice);
+            } else if (clickedItem.dataset.count === '0') {
+                return;
+            } else {
+                alert(`잔액이 부족합니다`);
             }
         });
     }
